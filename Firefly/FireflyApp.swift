af8423aa573f4cc006fc10a6a7f4135f09d212b3
Aditya Srinivasa <sirinivasa.at@impl.co.jp>
2023-10-09 16:28:27 +0900

    Initial Commit

---
+++ Firefly/FireflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireflyApp.swift
+//  Firefly
+//
+//  Created by Aditya Srinivasa on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct FireflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
